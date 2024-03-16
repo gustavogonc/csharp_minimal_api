f8cbb1f1cfc6b905359c6b79ffb66ada7676c3eb
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-03-16 17:19:15 -0300

    Adicionado relacionamento um para muitos com tags em produto

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -98,6 +98,14 @@ public class Category
     public int Id { get; set; }
     public string Name { get; set; }
 }
+
+public class Tag
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int ProductId { get; set; }
+}
+
 public class Product
 {
     public int Id { get; set; }
@@ -106,6 +114,7 @@ public class Product
     public string Description { get; set; }
     public int CategoryId { get; set; }
     public Category Category { get; set; }
+    public List<Tag> Tags { get; set; }
 }
 
 public class ApplicationDbContext: DbContext
