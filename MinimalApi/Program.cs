cbfa30da98f5f4caa12f392f84f1a6e80d478a40
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-03-15 21:35:30 -0300

    Adicionado DbContext para se conectar ao SQL Server

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<ApplicationDbContext>();
 
 var configuration = app.Configuration;
 ProductRepository.Init(configuration);
@@ -93,4 +96,13 @@ public class Product
 {
     public string Code { get; set; }
     public string Name { get; set; }
+}
+
+public class ApplicationDbContext: DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Server=localhost; Database=Products; User Id=sa; Password=<@Sql2022>;MultipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES");
+    
 }
