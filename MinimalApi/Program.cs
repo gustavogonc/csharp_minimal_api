db0f2d03ac89d4ab15e86c1c77126c4d721ff416
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-03-16 17:08:36 -0300

    Adicionado migrations com customização de propriedades da tabela

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -104,6 +104,15 @@ public class ApplicationDbContext: DbContext
 {
     public DbSet<Product> Products { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Description).HasMaxLength(500).IsRequired(false);
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Name).HasMaxLength(120).IsRequired();
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Code).HasMaxLength(20).IsRequired();
+    }
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlServer("Server=localhost; Database=Products; User Id=sa; Password=<@Sql2022>;MultipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES");
     
